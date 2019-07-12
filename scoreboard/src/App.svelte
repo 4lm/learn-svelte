47326fb692f8f3673ec4a3014aa6ba9c623d31d4
Alexis Michaltsis <alexis@michaltsis.net>
2019-07-12 15:48:26 +0200

    Add controls toggle switch

--- scoreboard/src/App.svelte
+++ scoreboard/src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   let name = "John Doe";
   let points = 100;
+  let showControls = false;
 
-  const addPoint = () => points += 1;
-  const removePoint = () => points -= 1;
+  const addPoint = () => (points += 1);
+  const removePoint = () => (points -= 1);
+  const toggleControls = () => (showControls = !showControls);
 </script>
 
 <style>
@@ -12,16 +14,21 @@
   }
 
   h3 {
-	margin-bottom: 10px;
+    margin-bottom: 10px;
   }
 </style>
 
 <div class="container">
   <div class="card">
-    <h1>{name}</h1>
+    <h1>
+      {name}
+      <button class="btn btn-sm" on:click={toggleControls}>+</button>
+    </h1>
     <h3>{points}</h3>
-	<button class="btn" on:click={addPoint}>+1</button>
-	<button class="btn btn-dark" on:click={removePoint}>-1</button>
-	<input type="number" bind:value={points} />
+    {#if showControls}
+      <button class="btn" on:click={addPoint}>+1</button>
+      <button class="btn btn-dark" on:click={removePoint}>-1</button>
+      <input type="number" bind:value={points} />
+    {/if}
   </div>
 </div>
